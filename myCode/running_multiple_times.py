48b8f76af29357ea304656937de91af3f2f87269
jsobolew <qba.sobolewski@gmail.com>
2023-04-27 15:54:17 +0200

    ran noise TaskIL

--- myCode/running_multiple_times.py
+++
@@ -1,4 +0,0 @@
-from samllNetCL_ClassIL import main
-
-for i in range(5):
-    main(i)
--- myCode/samllNetCL_TaskIL_Noise_Rehersal.py
+++ myCode/samllNetCL_TaskIL_Noise_Rehersal.py
@@ -8,7 +8,7 @@ import torch.optim as optim
 import wandb
 import torch.nn.functional as F
 
-def train_validation_all_classes(model, optimizer, tasks, rehersal_loader, epoch=1, log_interval = 1000):
+def train_validation_all_classes(model, optimizer, tasks, device, rehersal_loader, epoch=1, log_interval = 1000):
     train_losses = []
     tasks_acc = [[], [], [], [], []]
     exemplers = []
@@ -20,40 +20,41 @@ def train_validation_all_classes(model, optimizer, tasks, rehersal_loader, epoch
         for batch_idx, (data, target) in enumerate(tasks[taskNo]):
 
             # training on task
-            optimizer.zero_grad()
-            output = model(taskNo, data)
-            loss = F.cross_entropy(output, target)
+            output = model(taskNo, data.to(device))
+            loss = F.cross_entropy(output, target.to(device))
 
             # noise rehersal
             rehersal_data = next(rehersal_iterator)
-            output = model(taskNo, rehersal_data[0])
-            loss += F.cross_entropy(output, rehersal_data[1])
+            output = model(taskNo, rehersal_data[0].to(device))
+            loss += F.cross_entropy(output, rehersal_data[1].to(device))
+
+            optimizer.zero_grad()
             loss.backward()
             optimizer.step()
 
             if batch_idx % log_interval == 0:
                 print(f"Train [{batch_idx * len(data)} / {len(tasks[taskNo].dataset)}]       loss: {loss.item()}")
                 for i in range(len(tasks)):
-                    curr_task_acc = test(model, tasks[i], i, print_accuracy=False)
+                    curr_task_acc = test(model, tasks[i], i, device, print_accuracy=False)
                     tasks_acc[i].append(curr_task_acc)
                     wandb.log({f"acc_task_{i}": curr_task_acc})
 
-            train_losses.append(loss.item())
+            train_losses.append(loss.cpu().item())
             wandb.log({"loss": loss.item()})
             exemplars = tasks[taskNo].batch_size * batch_idx
             exemplers.append(exemplars)
             wandb.log({"exemplers": exemplers*2})
     return train_losses, tasks_acc, exemplers
 
-def test(model, test_loader, taskNo, print_accuracy=True):
+def test(model, test_loader, taskNo, device, print_accuracy=True):
     model.eval()
     test_loss = 0
     correct = 0
     with torch.no_grad():
         for data, target in test_loader:
-            output = model(taskNo, data)
-            test_loss += F.cross_entropy(output, target, size_average=False).item()
-            pred = output.data.max(1, keepdim=True)[1]
+            output = model(taskNo, data.to(device))
+            test_loss += F.cross_entropy(output, target.to(device), size_average=False).item()
+            pred = output.data.max(1, keepdim=True)[1].cpu()
             correct += pred.eq(target.data.view_as(pred)).sum()
     test_loss /= len(test_loader.dataset)
     if print_accuracy:
@@ -92,16 +93,16 @@ def main():
         config={
         "setup": "task IL",
         "learning_rate": 0.1,
-        "architecture": "CNN",
+        "architecture": "NetTaskIL",
         "dataset": "MNIST",
         "epochs": 1,
         }
     )
 
-    model = NetTaskIL(10)
+    model = NetTaskIL(10).to(device)
     optimizer = optim.SGD(model.parameters(), lr=0.1)
 
-    train_losses, tasks_losses, exemplers = train_validation_all_classes(model, optimizer, tasks, rehersal_loader, epoch=1, log_interval = 10)
+    train_losses, tasks_losses, exemplers = train_validation_all_classes(model, optimizer, tasks, device, rehersal_loader, epoch=1, log_interval = 10)
 
 if __name__ == "__main__":
     main()
