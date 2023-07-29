e7d3bc9b1b9535827b45d7eb3653c58b1a200660
jsobolew <qba.sobolewski@gmail.com>
2023-07-29 15:33:04 +0200

    updated runnig script and small refector

--- myCode/script.sh
+++ myCode/script.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-for i in 1 2 3 4 5
-do
-   /home/jsobolewski/anaconda3/envs/my_env/bin/python /home/jsobolewski/mgr/myCode/samllNetCL_TaskIL_Noise_Rehersal.py
-   # C:/Users/QbaSo/AppData/Local/Programs/Python/Python39/python.exe "c:/Users/QbaSo/Desktop/praca magisterska/myCode/samllNetCL_TaskIL.py"
-done
+python_ref=$PYTHON_INTERPRETER
 
+rehearsal_datasets=('dead_leaves-mixed', 'dead_leaves-oriented', 'dead_leaves-squares', 'dead_leaves-textures', 'stat-spectrum', 'stat-spectrum_color', 'stat-spectrum_color_wmm', 'stat-wmm', 'stylegan-highfreq', 'stylegan-random')
+for rehearsal_dataset in rehearsal_datasets
+do
+  for i in 1 2 3 4 5
+  do
+    python_ref run_expoeriment.py rehearsal_dataset=rehearsal_dataset
+  done
+done
