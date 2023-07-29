66abbd3daca3d1195479181aba00e7c393a56f96
jsobolew <qba.sobolewski@gmail.com>
2023-07-29 15:10:22 +0200

    some changes

--- myCode/run_experiment.py
+++ myCode/run_experiment.py
@@ -1,6 +1,4 @@
 import hydra
-# from omegaconf import DictConfig, OmegaConf
-import torch
 from Nets import SmallAlexNetTaslIL
 from taskIL import train_validation_all_classes
 from dataloaders.cifar10 import cifar10_for_classes_TaskIL
@@ -8,17 +6,15 @@ from dataloaders.noise import dataloader_pretraining
 import numpy as np
 import torch.optim as optim
 import wandb
-import torch.nn.functional as F
 from utils import get_device
 
 model_dict = {
-    "SmallAlexNetTaslIL" : SmallAlexNetTaslIL
+    "SmallAlexNetTasklIL": SmallAlexNetTaslIL
 }
 
 
-
 @hydra.main(version_base=None, config_path="configs/experiments", config_name="AlexNetTaskILNoise")
-def main(cfg ) -> None:
+def main(cfg) -> None:
     device = get_device()
 
     # ------
@@ -32,22 +28,22 @@ def main(cfg ) -> None:
     task4 = cifar10_for_classes_TaskIL(classes.pop(), classes.pop())
     task5 = cifar10_for_classes_TaskIL(classes.pop(), classes.pop())
 
-    tasks = [task1 ,task2, task3, task4, task5]
+    tasks = [task1, task2, task3, task4, task5]
     # ------
 
-    rehersal_loader = dataloader_pretraining(cfg['rehersal_dataset'], no_classes=2)
+    rehearsal_dataset = dataloader_pretraining(cfg['rehearsal_dataset'], no_classes=2)
 
     wandb.init(
         # set the wandb project where this run will be logged
         project=cfg['project'],
-        
+
         # track hyperparameters and run metadata
         config={
-        "setup": cfg['setup'],
-        "learning_rate": cfg['learning_rate'],
-        "architecture": cfg['architecture'],
-        "dataset": cfg['dataset'],
-        "epochs": cfg['epochs'],
+            "setup": cfg['setup'],
+            "learning_rate": cfg['learning_rate'],
+            "architecture": cfg['architecture'],
+            "dataset": cfg['dataset'],
+            "epochs": cfg['epochs'],
         },
         mode="disabled"
     )
@@ -57,7 +53,8 @@ def main(cfg ) -> None:
 
     optimizer = optim.SGD(model.parameters(), lr=cfg['learning_rate'])
 
-    train_validation_all_classes(model, optimizer, tasks, device, rehersal_loader, epoch=cfg['epochs'], log_interval = 10)
+    train_validation_all_classes(model, optimizer, tasks, device, rehearsal_dataset, epoch=cfg['epochs'], log_interval=10)
+
 
 if __name__ == "__main__":
     main()
