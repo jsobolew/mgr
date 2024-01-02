b89063e1a016bd428266c1c67d479f7710bd0b37
jsobolew <qba.sobolewski@gmail.com>
2024-01-03 00:45:52 +0100

    visualization changes, added boxplot logic

--- myCode/Results visualization/visualization.py
+++ myCode/Results visualization/visualization.py
@@ -25,14 +25,15 @@ class Visualization:
             runs_list.append(run.config)
         self.df = pd.DataFrame(runs_list).astype(str)
 
-        faulty_runs = []
+        # find faulty runs
+        self.faulty_runs = []
         for i in range(len(self.df)):
             if self.df.iloc[i].classes_list != 'nan':
                 classes_list = json.loads(self.df.iloc[i].classes_list)
                 for task in classes_list:
                     if 0 in task:
                         if task[1] == 0:
-                            faulty_runs.append(i)
+                            self.faulty_runs.append(i)
 
         # UID
         self.df['UID'] = ''
@@ -53,13 +54,130 @@ class Visualization:
                 idx = idx & (self.df[c] == v)
 
             idxs = self.df[idx].index
-            idxs = [idx for idx in idxs if idx not in faulty_runs]
+            idxs = [idx for idx in idxs if idx not in self.faulty_runs] # remove faulty runs
             self.unique_run_settings_idxs.append(idxs)
 
         # accuracy axis range for plotting
         self.y_min = 45
         if self.df.setup[0] == 'classIL':
             self.y_min = 0
+
+        # run unique params - run idxs dict
+        self.runs_params_settings_idxs_dict = {}
+        for i, run_param in enumerate(self.unique_run_params):
+            self.runs_params_settings_idxs_dict["-".join(run_param)] = self.unique_run_settings_idxs[i]
+
+    def box_plot(metric_name, df):
+        UIDS = df['UID'].unique()
+
+        values = []
+        for uid in UIDS:
+            values.append(df[df['UID']==uid][metric_name].median())
+
+        order = np.argsort(values)
+        UIDS = UIDS[order]
+
+        plt.figure(figsize=(20,10))
+        for i, uid in enumerate(UIDS):
+            plt.boxplot(df[df['UID']==uid][metric_name], positions=[i], widths=0.6)
+
+        # labels = [f"{uid.split(';')[0]} {uid.split(';')[2]} {uid.split(';')[4]}" for uid in UIDS]
+        labels = UIDS
+
+        plt.xticks(np.arange(len(UIDS)), labels, rotation=90)
+        plt.title(f"metic: {metric_name}       label convention: dataset pretraining? epochs")
+        plt.show()
+
+    def extract_all_runs_metrics(self):
+        self.runs_metrics = []
+        for i in range(len(self.df)):
+            if i in self.faulty_runs:
+                continue
+            try:
+                UID = self.df.iloc[i]['UID']
+                run = self.runs[i].history(100000)
+
+                df_train = run[self.acc_col].dropna().reset_index().drop(columns='index')
+                df_test = run[self.acc_test_col].dropna().reset_index().drop(columns='index')
+
+                # accuracy metrics table
+                num_tasks = len(self.acc_test_col)
+                steps_per_task = len(df_test) // num_tasks
+                acc_at_the_end = df_test.iloc[-1].values
+                acc_max = df_test.max().values
+                acc_min = df_test.min().values # todo would be good to change to min accuracy after training
+                acc_mean = []
+                for i in range(num_tasks):
+                    acc_mean.append(df_test[self.acc_test_col[i]].dropna()[steps_per_task*(i):].mean())
+
+                acc_decrease = (df_test.max().values - acc_at_the_end)
+                forgeting_tasks = np.arange(num_tasks-1, -1, -1)
+                forgeting_tasks[forgeting_tasks == 0] = 10e6 # during the last task there is no time to forget and no time to divide by 0
+                acc_mean_decrease_per_task = acc_decrease / forgeting_tasks
+
+                curr_dict = {}
+                curr_dict["UID"] = UID
+
+                for task in range(num_tasks):
+                    curr_dict[f"acc_at_the_end_task_{task}"] = acc_at_the_end[task]
+                    curr_dict[f"acc_mean_task_{task}"] = acc_mean[task]
+                    curr_dict[f"acc_mean_decrease_per_task_{task}"] = acc_mean_decrease_per_task[task]
+                    curr_dict[f"acc_max{task}"] = acc_max[task]
+                    curr_dict[f"acc_min{task}"] = acc_min[task]
+
+                curr_dict["split"] = "test"
+
+                self.runs_metrics.append(curr_dict)
+            except:
+                print(f"Could not fetch metrics in run: {i} UID: {UID}")
+
+        self.metrics_df = pd.DataFrame(self.runs_metrics)
+        for i, params in enumerate(self.metrics_df .UID.apply(lambda x: x.split(';'))):
+            for c, param in zip(self.UID, params):
+                self.metrics_df .loc[i, c] = param
+
+
+
+    def print_settings(self):
+        for i, run_param in enumerate(self.unique_run_params):
+            print(f"Setting {i}: {run_param} num runs: {len(self.unique_run_settings_idxs[i])}")
+
+    def plot_single_setting(self, run_param):
+        unixe_idxs = self.runs_params_settings_idxs_dict["-".join(run_param)]
+
+        for i, run_idx in enumerate(unixe_idxs):
+            if i in self.faulty_runs:
+                continue
+            curr_run = self.runs[run_idx].history(100000)
+            curr_run['run_no'] = i
+
+            df_train = curr_run[self.acc_col].dropna().reset_index().drop(columns='index')
+            df_test = curr_run[self.acc_test_col].dropna().reset_index().drop(columns='index')
+
+            fig, ax = plt.subplots(2, 1)
+            fig.set_figheight(10)
+            fig.set_figwidth(10)
+
+            title = ""
+            for additional_param in self.additional_title_params:
+                value = self.df.iloc[unixe_idxs][additional_param].iloc[0]
+                title += f"{additional_param}: {value}\n"
+            unique_params = self.df.iloc[unixe_idxs]['UID'].iloc[0].split(";")[:-1]
+            for name, value in zip(self.UID, unique_params):
+                title += f"{name}: {value}\n"
+
+            fig.suptitle(title + f"runs: {len(unixe_idxs)}", fontsize=12, position=(0.5, 1.06))
+
+            # train
+            df_train.plot(ax=ax[0], grid=True, ylim=[self.y_min, 100], title='Train')
+            ax[0].set_xlabel("step")
+            ax[0].set_ylabel("accuracy [%]")
+
+            # test
+            df_test.plot(ax=ax[1], grid=True, ylim=[self.y_min, 100], title='Test')
+            ax[1].set_xlabel("step")
+            ax[1].set_ylabel("accuracy [%]")
+            plt.show()
 
     def plot_everything(self):
         for unixe_idxs in self.unique_run_settings_idxs:
